aa7ef819b9b62ab7069b428ad1915eedffb84442
chayan <admin@ditskill.com>
2023-06-24 01:21:11 +0600

    fetch data success

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+const [city,setCity]=useState();
+const [weather,setWeather]=useState();
+const search = (e) => {
+  if (e.key === 'Enter') {
+    const city = e.target.value; // Assuming you have an input field where the user enters the city name
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${city}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data);
+      
+        console.log(weather);
+      })
+      .catch((error) => {
+        console.error('Error fetching weather data:', error);
+      });
+
+
+      fetch(`https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=${city}`)
+      .then(res=>res.json())
+      .then(data=>console.log(data))
+
+  }
+};
+
+console.log(city);
+
+
+
+ 
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>lets build weather api</h1>
+    <input type="text" placeholder="Search" 
+    onChange={(e)=>setCity(e.target.value)}
+    value={city}
+    onKeyDown={search}
+    />
+{
+  weather?.current ? weather.current.condition.text : "Your city name is not correct"
+}
+
+<div>
+  <button className="btn btn-primary">to get 7 days weather</button>
+</div>
+      
     </>
   )
 }
